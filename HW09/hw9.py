32e3bdd915f84cd04edc57e877650960c2e9ebd8
zzh <108445240+X0X0X00@users.noreply.github.com>
2024-07-24 17:16:14 +0800

    Add files via upload

---
+++ HW09/hw9.py
@@ -0,0 +1,58 @@
+# Zhenhao Zhang 11/21/23
+
+
+# Q1
+
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+my_dict['d'] = 4
+# print(my_dict)
+
+# Q2
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+car["year"] = 1966
+# print(car)
+
+
+# Q3
+
+fruits = {
+    'apple': 'red',
+    'mango': 'orange',
+    'orange': 'orange',
+    'grape': 'purple',
+    'kiwi': 'brown'
+}
+
+# for ele in fruits:
+#     print(ele)
+
+
+
+
+# Q4
+
+fruits = {
+    'apple': 'red',
+    'mango': 'orange',
+    'orange': 'orange',
+    'grape': 'purple',
+    'kiwi': 'brown'
+}
+del fruits['kiwi'], fruits['grape']
+
+# for ele in fruits:
+#     print(ele)
+
+
+# Q5
+
+
+
+# Q6
+
+print(len(my_dict))
